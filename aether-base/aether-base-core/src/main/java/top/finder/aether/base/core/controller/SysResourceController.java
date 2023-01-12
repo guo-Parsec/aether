bb1efb1135fcd286d9e235ab62b655fa9103cee0
guochengqiang <guochengqiang@erayt.com>
2023-01-12 10:12:33 +0800

    资源调整

--- aether-base/aether-base-core/src/main/java/top/finder/aether/base/core/controller/SysResourceController.java
+++ aether-base/aether-base-core/src/main/java/top/finder/aether/base/core/controller/SysResourceController.java
@@ -1,14 +1,10 @@
 package top.finder.aether.base.core.controller;
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.finder.aether.base.api.support.pool.BaseApiConstantPool;
 import top.finder.aether.base.core.service.SysResourceService;
-import top.finder.aether.common.support.api.Apis;
 
 /**
  * <p>系统资源管理</p>
@@ -24,12 +20,5 @@ public class SysResourceController {
 
     public SysResourceController(SysResourceService service) {
         this.service = service;
-    }
-
-    @ApiOperation(value = "自动生成资源", notes = "自动生成资源")
-    @PostMapping(value = "/auto-generate/{appName}")
-    public Apis<Void> autoGenerateResource(@PathVariable("appName") String appName) {
-        service.autoGenerateResource(appName);
-        return Apis.success();
     }
 }
